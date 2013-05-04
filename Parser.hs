f29e374f36d386a46a6d4704f3a8794ba9108260
Eric Reed <ecreed@cs.washington.edu>
2013-05-04 15:35:26 -0700

    changed liftMx uses into applicative style

--- Parser.hs
+++ Parser.hs
@@ -4,13 +4,13 @@ module Parser (program, readProgram) where
 
 import SIF
 import Text.Parsec
-import Control.Monad
+import Control.Applicative hiding (Alternative(..))
 
 -- parser function
 readProgram = parse program ""
 
 sign	  = option 1 (char '-' >> return (-1))
-num	  = liftM2 (*) sign (liftM read $ many1 digit) <?> "integer"
+num	  = (*) <$> sign <*> (read <$> many1 digit) <?> "integer"
 offset	  = num <?> "offset"
 size	  = num <?> "size"
 location  = num <?> "location"
@@ -20,27 +20,29 @@ colon	= char ':'
 at	= char '@'
 aspace	= char ' '
 star	= char '*'
+parens = between (char '(') (char ')')
+brackets = between (char '[') (char ']')
 
 idFirst = letter <|> char '_'
 idRest	= idFirst <|> digit
 
-identiferSuffix suf = liftM2 (:) idFirst (manyTill idRest $ try suf)
+identiferSuffix suf = (:) <$> idFirst <*> (manyTill idRest $ try suf)
 
-identifier = liftM2 (:) idFirst (many idRest)
+identifier = (:) <$> idFirst <*> many idRest
 
-varWithOffset suf v = liftM2 v (identiferSuffix suf) offset
+varWithOffset suf v = v <$> identiferSuffix suf <*> offset
 
 -- operands
 global	      = string "GP" >> return Global
 frame	      = string "FP" >> return Frame
-constant      = liftM Constant num
+constant      = Constant <$> num
 address	      = varWithOffset (string "_base#") Address
 staticField   = varWithOffset (string "_offset#") StaticField
-dynamicField  = liftM DynamicField $ identiferSuffix (string "_offset#?")
-stack	      = liftM2 Stack identifier (hash >> offset)
-register      = liftM Register $ between (char '(') (char ')') location 
-typeuse	      = liftM2 Type (identiferSuffix $ string "_type#") size
-codelabel     = liftM Label $ between (char '[') (char ']') location
+dynamicField  = DynamicField <$> identiferSuffix (string "_offset#?")
+stack	      = Stack <$> identifier <*> (hash >> offset)
+register      = Register <$> parens location 
+typeuse	      = Type <$> (identiferSuffix $ string "_type#") <*> size
+codelabel     = Label <$> brackets location
 
 operand = choice (map try [global, frame, constant, address, staticField, dynamicField, stack, register, typeuse, codelabel]) <?> "operand"
 
@@ -50,10 +52,10 @@ unboxBool   = string "bool" >> return UnboxBool
 boxInt	    = string "Integer" >> return BoxInt
 boxBool	    = string "Boolean" >> return BoxBool
 boxList	    = string "List" >> return List
-boxClass    = liftM Class identifier
+boxClass    = Class <$> identifier
 boxDynamic  = string "dynamic" >> return Dynamic
 baseType = choice . map try $ [unboxInt, unboxBool, boxInt, boxBool, boxList, boxClass, boxDynamic]
-pointer = liftM2 f baseType (many star) <?> "type"
+pointer = f <$> baseType <*> many star <?> "type"
   where f t s = foldl1 (.) (id : map (const Pointer) s) t
 typesig = colon >> pointer 
 
@@ -61,16 +63,16 @@ spaceOp = aspace >> operand
 spaceType = aspace >> typesig
 
 -- SIF side effect opcodes
-call = liftM Call (string "call" >> spaceOp)
-store = liftM2 Store (string "store" >> spaceOp) spaceOp
-move = liftM2 Move (string "move" >> spaceOp) spaceOp
-checkbounds = liftM2 Checkbounds (string "checkbounds" >> spaceOp) spaceOp
-storedynamic = liftM3 StoreDynamic (string "stdynamic" >> spaceOp) spaceOp spaceOp
-write = liftM Write (string "write" >> spaceOp)
+call = Call <$> (string "call" >> spaceOp)
+store = Store <$> (string "store" >> spaceOp) <*> spaceOp
+move = Move <$> (string "move" >> spaceOp) <*> spaceOp
+checkbounds = Checkbounds <$> (string "checkbounds" >> spaceOp) <*> spaceOp
+storedynamic = StoreDynamic <$> (string "stdynamic" >> spaceOp) <*> spaceOp <*> spaceOp
+write = Write <$> (string "write" >> spaceOp)
 wrl = string "wrl" >> return Newline
-enter = liftM Enter (string "enter" >> spaceOp)
-ret = liftM Ret (string "ret" >> spaceOp)
-param = liftM Param (string "param" >> spaceOp)
+enter = Enter <$> (string "enter" >> spaceOp)
+ret = Ret <$> (string "ret" >> spaceOp)
+param = Param <$> (string "param" >> spaceOp)
 entrypc = string "entrypc" >> return Entrypc
 sideeffect = choice . map try $ [call, store, move, checkbounds, storedynamic, write, wrl, enter, ret, param, entrypc]
 
@@ -99,28 +101,28 @@ binary = choice . map try $ [add, sub, mul, divide, modulo, eq, leq, lt, istype,
 
 -- SIF branch opcodes
 jump = string "br" >> return Jump
-ifzero = liftM IfZero $ string "blbc" >> spaceOp
-ifset = liftM IfSet $ string "blbs" >> spaceOp
+ifzero = IfZero <$> (string "blbc" >> spaceOp)
+ifset = IfSet <$> (string "blbs" >> spaceOp)
 branch = choice . map try $ [jump, ifzero, ifset]
 
 -- SIF opcodes
 nop = string "nop" >> return NOP
-sideeffectopcode = liftM SideEffect sideeffect
-unaryopcode = liftM3 Unary unary spaceOp spaceType
-binaryopcode = liftM4 Binary binary spaceOp spaceOp spaceType
-branchopcode = liftM2 Branch branch spaceOp
+sideeffectopcode = SideEffect <$> sideeffect
+unaryopcode = Unary <$> unary <*> spaceOp <*> spaceType
+binaryopcode = Binary <$> binary <*> spaceOp <*> spaceOp <*> spaceType
+branchopcode = Branch <$> branch <*> spaceOp
 opcode = choice (map try [sideeffectopcode, unaryopcode, binaryopcode, branchopcode, nop]) <?> "opcode"
 
 -- variable list
-varDecl = liftM3 SIFVarDecl identifier (hash >> size) typesig
+varDecl = SIFVarDecl <$> identifier <*> (hash >> size) <*> typesig
 varDeclList = sepBy1 varDecl aspace
 
 -- top level stuff
-typeDecl = liftM2 SIFTypeDecl (string "type" >> aspace >> identifier) (colon >> aspace >> varDeclList) <?> "type declaration"
-methodDecl = liftM3 SIFMethodDecl (string "method" >> aspace >> identifier) (at >> location) (colon >> option [] (aspace >> varDeclList)) <?> "method declaration"
-globalDecl = liftM3 SIFGlobalDecl (string "global" >> aspace >> identifier) (hash >> offset) typesig <?> "global declaration"
-instruction = liftM2 SIFInstruction (string "instr" >> aspace >> location) (colon >> aspace >> opcode) <?> "instruction"
+typeDecl = SIFTypeDecl <$> (string "type" >> aspace >> identifier) <*> (colon >> aspace >> varDeclList) <?> "type declaration"
+methodDecl = SIFMethodDecl <$> (string "method" >> aspace >> identifier) <*> (at >> location) <*> (colon >> option [] (aspace >> varDeclList)) <?> "method declaration"
+globalDecl = SIFGlobalDecl <$> (string "global" >> aspace >> identifier) <*> (hash >> offset) <*> typesig <?> "global declaration"
+instruction = SIFInstruction <$> (string "instr" >> aspace >> location) <*> (colon >> aspace >> opcode) <?> "instruction"
 
 -- the whole program
 lots x = sepEndBy x (newline >> spaces)
-program = liftM4 SIFProgram (spaces >> lots typeDecl) (lots methodDecl) (lots globalDecl) (lots instruction)
+program = SIFProgram <$> (spaces >> lots typeDecl) <*> (lots methodDecl) <*> (lots globalDecl) <*> (lots instruction)
