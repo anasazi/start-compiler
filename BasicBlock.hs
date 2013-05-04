31cce916c0561a38c715230844cbfb22bf977163
Eric Reed <ecreed@cs.washington.edu>
2013-05-04 05:58:53 -0700

    added exit information to the BasicBlock

--- BasicBlock.hs
+++ BasicBlock.hs
@@ -2,14 +2,15 @@
 module BasicBlock 
 ( BasicBlock, empty
 , leader, end
-, falls, jumpsTo, label
+, fallsTo, jumpsTo, label, locs
 , toBlocks, fromBlocks
 ) where
 
 import InstructionSet
 import Data.Function (on)
 import Data.List (nub, groupBy)
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, fromJust)
+import Control.Arrow
 
 {- A non-empty straight-line sequence of instructions without control 
  - flow between them. The first instruction (leader) is 
@@ -17,41 +18,64 @@ import Data.Maybe (catMaybes)
  - The last instruction (exit) is a) a branch or b) the instruction 
  - before a jump target.
  -}
-newtype BasicBlock i = BB { runBB :: [i] } deriving (Eq, Show)
-instance Functor BasicBlock where
-  fmap f = BB . fmap f . runBB
+data BasicBlockExit = 
+    Return
+  | Fall Integer 
+  | Jump Integer 
+  | Branch Integer Integer 
+  deriving (Eq, Show)
 
-leader = head . runBB
-end = last . runBB
+data BasicBlock i = BB [i] BasicBlockExit deriving (Eq, Show)
+instance Functor BasicBlock where
+  fmap f (BB is ex) = BB (fmap f is) ex
+
+leader (BB is _) = head is
+end (BB is _) = last is
+exit (BB _ ex) = ex
 
 -- can this block fall off the end?
-falls = canFall . end
+fallsTo (BB _ (Fall x)) = Just x
+fallsTo (BB _ (Branch _ x)) = Just x
+fallsTo _ = Nothing
 -- where does this block jump to?
-jumpsTo = jumpTarget . end
+jumpsTo (BB _ (Jump x)) = Just x
+jumpsTo (BB _ (Branch x _)) = Just x
+jumpsTo _ = Nothing
 -- jump target label of this block
 label :: InstructionSet i => BasicBlock i -> Integer
 label = loc . leader
+locs (BB is _) = map loc is
 
-empty n = BB [nop n]
+empty n m = BB [nop n] (Fall m)
 
 -- Break an instruction stream into basic blocks
 toBlocks :: InstructionSet i => [i] -> [BasicBlock i]
-toBlocks is = map BB blockified
+toBlocks is = map wrap blockified
   where 
   leaders = nub $ entrypc ++ calls ++ jumps ++ falls
 	where 
 	entrypc = [ loc i | i <- is, isMain i ]
 	calls = [ l | i <- is, isCall i, let Just l = callTarget i ]
-	jumps = catMaybes [ jumpTarget i | i <- is, isJump i ]
+	jumps = catMaybes [ target i | i <- is, isJump i ]
 	falls = [ l | (i1,i2) <- zip is (drop 1 is), isJump i1, let l = loc i2 ]
   blockified = f . dropWhile (not . isLeader) $ is
 	where
 	isLeader :: InstructionSet i => i -> Bool
 	isLeader = (`elem` leaders) . loc
-	f :: InstructionSet i => [i] -> [[i]]
-	f [] = []
-	f (x:xs) = let (ys,zs) = break isLeader xs in (x : ys) : f zs
+	f :: InstructionSet i => [i] -> [([i],Maybe Integer)]
+	f [] = [([], Nothing)]
+	f (x:xs) = let (ys,zs) = break isLeader xs in ((x : ys), g zs) : f zs
+	    where
+	    g :: InstructionSet i => [i] -> Maybe Integer
+	    g [] = Nothing
+	    g (z:_) = Just $ loc z
+  wrap :: InstructionSet i => ([i], Maybe Integer) -> BasicBlock i
+  wrap (bb, next) | isRet $ last bb = BB bb Return
+		  | isBranch $ last bb = BB bb $ Branch (fromJust . target . last $ bb) (fromJust next)
+		  | isJump $ last bb = BB bb $ Jump (fromJust . target . last $ bb)
+		  | canFall $ last bb = BB bb $ Fall (fromJust next)
+		  | otherwise = error "impossible block ending"
 
 -- Unwrap and concatenate the basic blocks
 fromBlocks :: [BasicBlock i] -> [i]
-fromBlocks = concatMap runBB
+fromBlocks = concatMap (\(BB is _) -> is)
