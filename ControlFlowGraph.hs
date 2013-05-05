f03b3d4232d2bbe026674fdfbc636a25d3ae9f0f
Eric Reed <ecreed@cs.washington.edu>
2013-05-04 19:59:41 -0700

    dominators working

--- ControlFlowGraph.hs
+++ ControlFlowGraph.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module ControlFlowGraph 
-( Vertex
+( Vertex, Edge(..)
 , CFG, buildCFG, linearize
 , entry, vertices, blocks, edges
 , succs, preds
 , reachable
+, dominators, idominators
 ) where
 
 import InstructionSet
@@ -16,27 +17,63 @@ import qualified Data.List as L
 import Data.Maybe
 import Control.Arrow
 import Control.Monad
+import Control.Applicative
+import Data.Array
 
 newtype Vertex = Vertex Integer deriving (Eq, Ord, Enum, Show)
-data Edge = Jump Vertex | Fall Vertex deriving (Eq, Show)
+data Edge = Jump Vertex | Fall Vertex deriving (Eq, Ord, Show)
 data CFG i = CFG 
   { entry :: Vertex
   , blocks :: M.Map Vertex (BasicBlock i)
-  , edges :: M.Map Vertex (S.Set Vertex)
+  , edges :: M.Map Vertex (S.Set Edge)
   }
   deriving Show
-
-length' = fromIntegral . length
+instance Functor CFG where
+  fmap f (CFG entry blocks edges) = CFG entry (fmap (fmap f) blocks) edges
 
 vertices = M.keys . blocks
 
-succs :: CFG i -> Vertex -> S.Set Vertex
+goesTo (Jump x) = x
+goesTo (Fall x) = x
+
+edge jump _ (Jump x) = jump x
+edge _ fall (Fall x) = fall x
+
+succs :: CFG i -> Vertex -> S.Set Edge
 succs cfg v = (edges cfg) M.! v
 
-preds :: CFG i -> Vertex -> S.Set Vertex
-preds cfg v = S.fromList . fmap fst . onlyMySuccs $ allSuccs
-  where allSuccs = zip (vertices cfg) $ fmap (succs cfg) (vertices cfg)
-	onlyMySuccs = filter ((S.member v) . snd)
+preds :: CFG i -> Vertex -> S.Set Edge
+preds cfg v = S.fromList . swapAround . trimDown . predEntries $ (edges cfg)
+  where predEntries = M.filter (S.member v . S.map goesTo)
+	trimDown = M.map (S.filter ((==v) . goesTo))
+	swapAround = fmap (\(k,v) -> edge (const $ Jump k) (const $ Fall k) (head . S.toList $ v)) . M.toList
+
+fixEq :: Eq a => (a -> a) -> a -> a
+fixEq f v | v' == v   = v
+	  | otherwise = fixEq f v'
+  where v' = f v
+
+dominators :: CFG i -> M.Map Vertex (S.Set Vertex)
+dominators cfg = 
+  let ivs = zip [0..] $ entry cfg : L.delete (entry cfg) (vertices cfg)
+      i2v i = snd . head . filter ((==i) . fst) $ ivs
+      v2i v = fst . head . filter ((==v) . snd) $ ivs
+      n = fst . last $ ivs
+      ps i = S.toList . S.map (v2i . goesTo) . preds cfg . i2v $ i
+      dom0 = array (0,n) ((0,[0]) : [(i, [0..n]) | i <- [1..n]])
+      domStep dom = array (0,n) ((0,[0]) : [(i, f i) | i <- [1..n]])
+	where f n = L.union [n] $ foldr1 L.intersect $ map (dom !) (ps n)
+      dom = fixEq domStep dom0
+  in M.fromList . map (i2v *** S.fromList . map i2v) $ assocs dom
+
+head' [] = Nothing
+head' (x:_) = Just x
+
+idominators :: CFG i -> M.Map Vertex (Maybe Vertex)
+idominators cfg =
+  let doms = dominators cfg
+      idoms = M.mapWithKey (\v ds -> S.delete v ds) $ doms
+  in M.map (\ids -> head' . M.keys . M.filter (==ids) $ doms) idoms
 
 -- Given the basic blocks for a routine, build the CFG
 buildCFG :: (Show i, InstructionSet i) => [BasicBlock i] -> CFG i
@@ -44,11 +81,11 @@ buildCFG bs = reachable $ CFG entry blocks edges
   where 
   entry = Vertex 1
   vs = [entry..]
-  l2v = M.fromList $ concatMap (\(ls, v) -> [ (l,v) | l <- ls ]) $ zip (fmap locs bs) vs
+  l2v = M.fromList . concatMap (\(ls, v) -> [ (l,v) | l <- ls ]) $ zip (locs <$> bs) vs
   blocks = M.fromList $ zip vs bs
   edges = M.map (\b -> S.fromList . catMaybes $ [f b, g b]) blocks
-    where f b = fmap (l2v M.!) (fallsTo b)
-	  g = fmap (l2v M.!) . jumpsTo
+    where f b = (Fall . (l2v M.!)) <$> fallsTo b
+	  g = fmap (Jump . (l2v M.!)) . jumpsTo
 
 -- remove any unreachable blocks
 reachable :: CFG i -> CFG i
@@ -58,7 +95,7 @@ reachable cfg =
   in S.foldl removeVertex cfg work
   where unreachable [] unmarked = unmarked
 	unreachable (v:vs) unmarked = unreachable vs' unmarked'
-	  where newlyMarked = succs cfg v `S.intersection` unmarked
+	  where newlyMarked = S.map goesTo (succs cfg v) `S.intersection` unmarked
 		vs' = S.toList newlyMarked ++ vs
 		unmarked' = unmarked S.\\ newlyMarked
 
@@ -66,14 +103,14 @@ removeVertex (CFG entry blocks edges) v
   | v == entry = error "cannot remove the entry vertex"
   | otherwise = CFG entry blocks' edges'
       where blocks' = M.delete v blocks
-	    edges' = M.map (S.delete v) . M.delete v $ edges
+	    edges' = M.map (S.delete (Jump v) . S.delete (Fall v)) . M.delete v $ edges
 
 -- Given the CFG for a routine, organize the blocks in their linear order
 linearize :: InstructionSet i => CFG i -> [BasicBlock i]
 linearize cfg = map (blocks cfg M.!) ordering
   where 
     l2v = M.fromList . concatMap (\(v, b) -> [ (l,v) | l <- locs b ]) . M.toList . blocks $ cfg
-    fallsToV v = fmap (l2v M.!) $ fallsTo $ blocks cfg M.! v
+    fallsToV v = fmap (l2v M.!) . fallsTo $ blocks cfg M.! v
     next = map (id &&& fallsToV) (vertices cfg)
     starts = uncurry (L.\\) . second catMaybes . unzip $ next
     line s = case join $ lookup s next of Nothing -> [s] ; Just s' -> s : line s'
--- main.hs
+++ main.hs
@@ -8,6 +8,8 @@ import Data.Map
 import Control.Monad
 import Control.Arrow
 
+pp = print . pretty
+
 main = do
   input <- getContents
   let parsed = readProgram input
@@ -16,7 +18,20 @@ main = do
 printCFGs ast@(SIFProgram ts ms gs is)  = do
   let rs = routines ast
   let cfgs = fmap buildCFG rs
+  forM_ (toList cfgs) $ \(m, cfg) -> do
+    pp m
+    print "edges:"
+    mapM_ print $ toList $ edges cfg
+    print "doms:"
+    let doms = dominators cfg
+    mapM_ print $ toList doms
+    print "idoms:"
+    let idoms = idominators cfg
+    mapM_ print $ toList idoms
+    
+  {-
   let rs' = fmap (fromBlocks . linearize) cfgs
   let (ms', is') = second concat . unzip $ toList rs'
   let ast' = SIFProgram ts ms' gs is'
   print . pretty $ ast'
+  -}
