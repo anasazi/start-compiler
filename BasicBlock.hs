047ed13d799bccf1d4991bf168ad05fe42835cd9
Eric Reed <ecreed@cs.washington.edu>
2013-05-17 01:37:43 -0700

    Added a module for generic basic blocks

---
+++ BasicBlock.hs
@@ -0,0 +1,14 @@
+module BasicBlock where
+
+{-
+ - A non-empty straight-line sequence of instructions without control flow between them.
+ - The first instruction (leader) is a) the entrence of a method/program or b) a jump target.
+ - The last instruction (exit) is a) a branch or b) the instruction before a jump target.
+ -}
+newtype BasicBlock i = BB { runBB :: [i] } deriving Eq
+instance Functor BasicBlock where
+  fmap f = BB . fmap f . runBB
+
+leader, exit :: BasicBlock i -> i
+leader = head . runBB
+exit = last . runBB
