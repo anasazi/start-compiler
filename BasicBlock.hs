0cf6caabad13def4d9e3b8ea2ceeba715c806441
Eric Reed <ecreed@cs.washington.edu>
2013-05-17 02:05:50 -0700

    removed Eq from basic block

--- BasicBlock.hs
+++ BasicBlock.hs
@@ -5,10 +5,9 @@ module BasicBlock where
  - The first instruction (leader) is a) the entrence of a method/program or b) a jump target.
  - The last instruction (exit) is a) a branch or b) the instruction before a jump target.
  -}
-newtype BasicBlock i = BB { runBB :: [i] } deriving Eq
+newtype BasicBlock i = BB { runBB :: [i] } 
 instance Functor BasicBlock where
   fmap f = BB . fmap f . runBB
 
-leader, exit :: BasicBlock i -> i
 leader = head . runBB
 exit = last . runBB
