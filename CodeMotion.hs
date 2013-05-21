420ea7dbbec7b588734a802d3796162b4ce3f0c4
Eric Reed <ecreed@cs.washington.edu>
2013-05-21 00:20:07 -0700

    outlined two optional optimizations

---
+++ CodeMotion.hs
@@ -0,0 +1,12 @@
+module CodeMotion
+( codeMotion
+) where
+
+import ControlFlowGraph
+import StaticSingleAssignment
+
+data Hole = Hole
+hole = undefined
+
+codeMotion :: CFG SSAInstruction -> CFG SSAInstruction
+codeMotion = hole
---
+++ CopyPropagation.hs
@@ -0,0 +1,12 @@
+module CopyPropagation
+( copyPropagation
+) where
+
+import ControlFlowGraph
+import StaticSingleAssignment
+
+data Hole = Hole
+hole = undefined
+
+copyPropagation :: CFG SSAInstruction -> CFG SSAInstruction
+copyPropagation = hole
