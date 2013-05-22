e2878fcdf35f9bce2e32a8e70c5efba7a9377bf1
Eric Reed <ecreed@cs.washington.edu>
2013-05-21 19:25:51 -0700

    implemented control flow graph construction and linearization

--- ControlFlowGraph.hs
+++ ControlFlowGraph.hs
@@ -1,30 +1,80 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module ControlFlowGraph 
 ( Vertex
 , CFG, buildCFG, linearize
-, entry, exit, blocks, succs, preds
+, entry, vertices, blocks, edges
+, succs, preds
+, reachable
 ) where
 
 import InstructionSet
 import BasicBlock 
-import Data.Map
-import Data.Set
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.Maybe
+import Debug.Trace
 
-data Hole = Hole
-hole = undefined
-
-newtype Vertex = Vertex Integer deriving Eq
+newtype Vertex = Vertex Integer deriving (Eq, Ord, Enum, Show)
 data CFG i = CFG 
   { entry :: Vertex
-  , exit :: Vertex
-  , blocks :: Map Vertex (BasicBlock i)
-  , succs :: Map Vertex (Set Vertex)
-  , preds :: Map Vertex (Set Vertex) 
+  , blocks :: M.Map Vertex (BasicBlock i)
+  , edges :: M.Map Vertex (S.Set Vertex)
   }
+  deriving Show
+
+length' = fromIntegral . length
+
+vertices = M.keys . blocks
+
+succs :: CFG i -> Vertex -> S.Set Vertex
+succs cfg v = (edges cfg) M.! v
+
+preds :: CFG i -> Vertex -> S.Set Vertex
+preds cfg v = S.fromList . fmap fst . onlyMySuccs $ allSuccs
+  where allSuccs = zip (vertices cfg) $ fmap (succs cfg) (vertices cfg)
+	onlyMySuccs = filter ((S.member v) . snd)
 
 -- Given the basic blocks for a routine, build the CFG
-buildCFG :: InstructionSet i => [BasicBlock i] -> CFG i
-buildCFG = hole
+buildCFG :: (Show i, InstructionSet i) => [BasicBlock i] -> CFG i
+buildCFG bs = reachable $ CFG entry blocks edges
+  where 
+  entry = Vertex 1
+  vs = [entry..]
+  l2v = M.fromList $ zip (fmap label bs) vs
+  blocks = M.fromList $ zip vs bs
+  edges = M.mapWithKey (\v b -> S.fromList . catMaybes $ [f b v, g b]) blocks
+    where f b = if falls b then Just . succ else const Nothing
+	  g = fmap (l2v M.!) . jumpsTo
+
+-- remove any unreachable blocks
+reachable :: CFG i -> CFG i
+reachable cfg = 
+  let unmarked = S.delete (entry cfg) $ S.fromList (vertices cfg)
+      work = unreachable [entry cfg] unmarked
+  in S.foldl removeVertex cfg work
+  where unreachable [] unmarked = unmarked
+	unreachable (v:vs) unmarked = unreachable vs' unmarked'
+	  where newlyMarked = succs cfg v `S.intersection` unmarked
+		vs' = S.toList newlyMarked ++ vs
+		unmarked' = unmarked `S.difference` newlyMarked
+
+removeVertex (CFG entry blocks edges) v
+  | v == entry = error "cannot remove the entry vertex"
+  | otherwise = CFG entry blocks' edges'
+      where blocks' = M.delete v blocks
+	    edges' = M.map (S.delete v) . M.delete v $ edges
 
 -- Given the CFG for a routine, organize the blocks in their linear order
 linearize :: InstructionSet i => CFG i -> [BasicBlock i]
-linearize = hole
+linearize cfg = fmap (blocks cfg M.!) ordering
+  where 
+    ordering = let x = entry cfg in x : follows x
+    follows v = 
+      let b = blocks cfg M.! v
+      in if falls b 
+	 then case S.toList $ succs cfg v of 
+	    --[] -> let x = succ v in trace (show v) (if x `elem` vertices cfg then x : follows x else [])
+	    [x] -> x : follows x
+	    [x,y] -> if Vertex (fromJust (jumpsTo (blocks cfg M.! v))) == x then y : follows y else x : follows x
+	  else let x = succ v in if x `elem` vertices cfg then x : follows x else []
--- main.hs
+++ main.hs
@@ -1,8 +1,27 @@
 import SIF
 import Parser
 import Pretty
+import Routine
+import ControlFlowGraph
+import BasicBlock
+import Data.Map
+import Control.Monad
 
 main = do
   input <- getContents
   let parsed = readProgram input
-  either print (print . pretty) parsed
+  either print printCFGs parsed
+--  either print (print . pretty) parsed
+
+printCFGs ast = do
+  let rs = routines ast
+  let cfgs = toList $ fmap buildCFG rs
+  forM_ cfgs (\(m, cfg) -> 
+    do print m
+       print $ entry cfg
+       print $ vertices cfg
+       print $ edges cfg
+       print $ blocks cfg
+       let linbs = linearize cfg
+       print . pretty . Vertical $ fromBlocks linbs
+       putStrLn "")
